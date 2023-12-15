a2b5bb941533604662b40ab8461d8a180063830e
libienz <Leekh10195@gmail.com>
2023-12-15 13:06:47 +0900

    feat: MessageResolver 작성

---
+++ src/main/java/vendingmachine/view/MessageResolver.java
@@ -0,0 +1,35 @@
+package vendingmachine.view;
+
+import vendingmachine.dto.CoinsDto;
+
+public class MessageResolver {
+
+    private String resolveVendingMachineMoneyInputMessage() {
+        return "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    }
+    public String resolveInputAmount(int money) {
+        return "투입 금액: " + formatPrice(money);
+    }
+
+    public String resolveVendingMachineCoinsMessage(CoinsDto coinsDto) {
+        return "자판기가 보유한 동전\n" + resolveCoinsMessage(coinsDto);
+    }
+    public String resolveChangeMessage(CoinsDto coinsDto) {
+        return "잔돈\n" + resolveCoinsMessage(coinsDto);
+    }
+    private String formatPrice(int value) {
+        return String.format("%,d원", value);
+    }
+
+    private String formatCount(int count) {
+        return String.format("%d개", count);
+    }
+
+    private String resolveCoinsMessage(CoinsDto coinsDto) {
+        StringBuilder messageBuilder = new StringBuilder();
+        coinsDto.getCoinValueCountMap().forEach((coinValue, count) ->
+                messageBuilder.append(formatPrice(coinValue)).append(" - ").append(formatCount(count))
+        );
+        return messageBuilder.toString();
+    }
+}
