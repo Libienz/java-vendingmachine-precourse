6b8a9a8a14c7245fe8f9396149bdc8587b7c08e6
libienz <Leekh10195@gmail.com>
2023-12-15 12:35:11 +0900

    feat: 동전 가치 리스트 반환 메서드 추가

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -12,5 +16,13 @@ public enum Coin {
         this.amount = amount;
     }
 
-    // 추가 기능 구현
+    public static List<Integer> amountValues() {
+        return Arrays.stream(Coin.values())
+                .map(Coin::getAmount)
+                .collect(Collectors.toList());
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
