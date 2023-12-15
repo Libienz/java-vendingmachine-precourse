5a56f564eb22edae45757faf99c3915951a7f11f
libienz <Leekh10195@gmail.com>
2023-12-15 15:51:53 +0900

    fix: 잔돈은 반환된 정보만 출력하도록 수정

--- src/main/java/vendingmachine/view/MessageResolver.java
+++ src/main/java/vendingmachine/view/MessageResolver.java
@@ -15,7 +15,7 @@ public class MessageResolver {
         return "자판기가 보유한 동전\n" + resolveCoinsMessage(coinsDto);
     }
     public String resolveChangeMessage(CoinsDto coinsDto) {
-        return "잔돈\n" + resolveCoinsMessage(coinsDto);
+        return "잔돈\n" + resolveExistCoinsMessage(coinsDto);
     }
     private String formatPrice(int value) {
         return String.format("%d원", value);
@@ -30,6 +30,17 @@ public class MessageResolver {
         coinsDto.getCoinValueCountMap().forEach((coinValue, count) ->
                 messageBuilder.append(formatPrice(coinValue)).append(" - ").append(formatCount(count)).append("\n")
         );
+        return messageBuilder.toString();
+    }
+
+    private String resolveExistCoinsMessage(CoinsDto coinsDto) {
+        StringBuilder messageBuilder = new StringBuilder();
+        coinsDto.getCoinValueCountMap()
+                .forEach((coinValue, count) -> {
+                    if (count > 0) {
+                        messageBuilder.append(formatPrice(coinValue)).append(" - ").append(formatCount(count)).append("\n");
+                    }
+                });
         return messageBuilder.toString();
     }
 }
