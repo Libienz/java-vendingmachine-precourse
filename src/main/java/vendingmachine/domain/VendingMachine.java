ae416f61c13ab75b16dc30efe7fda45310a1ba49
libienz <Leekh10195@gmail.com>
2023-12-15 15:08:08 +0900

    feat: 잔돈 계산 기능 추가

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,12 +1,18 @@
 package vendingmachine.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendingMachine {
+    private static final String NOT_ENOUGH_INPUT_MESSAGE = "[ERROR] 투입 금액이 충분하지 않습니다";
     private final Coins coins;
     private final Items items;
+    private final Money money;
 
-    public VendingMachine(Coins coins, Items items) {
+    public VendingMachine(Coins coins, Items items, Money money) {
         this.coins = coins;
         this.items = items;
+        this.money = money;
     }
 
     public Items getItems() {
@@ -21,11 +27,29 @@ public class VendingMachine {
         return items.getItem(name);
     }
 
-    public boolean canAffordAtLeastOneItem(Money inputMoney) {
+    public boolean isMoneyCanAffordAtLeastOneItem(Money inputMoney) {
         return inputMoney.isAffordable(cheapestItemPrice());
     }
 
     public Price cheapestItemPrice() {
         return items.cheapestPrice();
+    }
+
+    public void purchaseItem(Money inputMoney, String itemName) {
+        Item item = getItem(itemName);
+        if (!inputMoney.isAffordable(item.getPrice())) {
+            throw new IllegalArgumentException(NOT_ENOUGH_INPUT_MESSAGE);
+        }
+        inputMoney.subtract(item.getPrice());
+    }
+
+    public Coins getChange() {
+        Coin coin;
+        Map<Coin, Integer> coinCountMap = new HashMap<>();
+        while ((coin = coins.getMostValuableCoinLessThanMoney(money)) != null) {
+            coinCountMap.put(coin, coinCountMap.getOrDefault(coin, 0) + 1);
+            money.subtract(new Price(coin.getAmount()));
+        }
+        return new Coins(coinCountMap);
     }
 }
