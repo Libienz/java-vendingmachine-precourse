6c923d7426bce47ad005829a41e4321c1107692d
libienz <Leekh10195@gmail.com>
2023-12-15 11:48:09 +0900

    feat: 자판기 판매 상품 Items 클래스 구현

---
+++ src/main/java/vendingmachine/Items.java
@@ -0,0 +1,13 @@
+package vendingmachine;
+
+public class Items {
+    private final String name;
+    private final Price price;
+    private final int quantity;
+
+    public Items(String name, Price price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
