9b37804bba5d7a6aa340b4afb74591a23c030902
libienz <Leekh10195@gmail.com>
2023-12-15 16:01:55 +0900

    fix: Money 클래스 검증 로직 추가

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -1,9 +1,12 @@
 package vendingmachine.domain;
 
 public class Money {
+    private static final String INVALID_RANGE_MONEY_MESSAGE = "[ERROR] 범위가 올바르지 않습니다";
+    private static final int MIN = 0;
     private int value;
 
     public Money(int value) {
+        validate(value);
         this.value = value;
     }
 
@@ -21,5 +24,15 @@ public class Money {
 
     public void add(Money money) {
         value += money.getValue();
+    }
+
+    private void validate(int value) {
+        validateRange(value);
+    }
+
+    private void validateRange(int value) {
+        if (value < MIN) {
+            throw new IllegalArgumentException(INVALID_RANGE_MONEY_MESSAGE);
+        }
     }
 }
