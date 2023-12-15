b98ce9af9aa45fdd161bd6028f30bf238ac0c2bd
libienz <Leekh10195@gmail.com>
2023-12-15 12:39:31 +0900

    feat: 가치로 동전을 판별하는 from 메서드 구현

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -10,6 +10,7 @@ public enum Coin {
     COIN_50(50),
     COIN_10(10);
 
+    private static final String NO_SUCH_COIN_MESSAGE = "[ERROR] 해당하는 동전을 찾을 수 없습니다";
     private final int amount;
 
     Coin(final int amount) {
@@ -20,6 +21,13 @@ public enum Coin {
         return Arrays.stream(Coin.values())
                 .map(Coin::getAmount)
                 .collect(Collectors.toList());
+    }
+
+    public static Coin from(int amount) {
+        return Arrays.stream(Coin.values())
+                .filter(coin -> coin.getAmount() == amount)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NO_SUCH_COIN_MESSAGE));
     }
 
     public int getAmount() {
