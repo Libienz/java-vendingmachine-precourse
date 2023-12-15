c289bf4f8418c9310eb5093a940813cfed5ff3ec
libienz <Leekh10195@gmail.com>
2023-12-15 11:59:08 +0900

    chore: Items -> Item 클래스 명 변경

--- src/main/java/vendingmachine/domain/Item.java
+++ src/main/java/vendingmachine/domain/Item.java
@@ -1,11 +1,11 @@
 package vendingmachine.domain;
 
-public class Items {
+public class Item {
     private final String name;
     private final Price price;
     private final int quantity;
 
-    public Items(String name, Price price, int quantity) {
+    public Item(String name, Price price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
